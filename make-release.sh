cfc43bacf8be0ab9fa0253d0a561d6ebdaa99e17
M. Hofman <m.hofman@chronos-technology.nl>
2018-02-21 15:06:56 +0100

    Will make sure correct version is used in make-release

--- make-release.sh
+++ make-release.sh
@@ -2,5 +2,5 @@ git checkout release
 git merge --no-ff master
 echo "export VERSION=$1" > VERSION
 git commit .
-git commit -am "Version bump to $VERSION"
-git tag -a $VERSION -m "$VERSION Release"
+git commit -am "Version bump to $1"
+git tag -a $1 -m "$1 Release"
