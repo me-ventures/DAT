e4741a2908738519077447c16a7e03233a868ba3
M. Hofman <m.hofman@chronos-technology.nl>
2018-03-11 14:57:29 +0100

    Basic implementation for StatsD metrics client

---
+++ DAT/Configuration/MetricsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace DAT.Configuration
+{
+    public class MetricsConfiguration
+    {
+        public string Type { get; set; }
+        
+        public string Hostname { get; set; }
+        
+        public int Port { get; set; }
+        
+        public string MetricsPrefix { get; set; }
+    }
+}
---
+++ DAT/Metrics/StatsD/StatsDMetricsClient.cs
@@ -0,0 +1,34 @@
+﻿using DAT.Configuration;
+using StatsdClient;
+
+namespace DAT.Metrics.StatsD
+{
+    public class StatsDMetricsClient : IMetricsClient
+    {
+        public StatsDMetricsClient(MetricsConfiguration configuration)
+        {
+           
+            StatsdClient.Metrics.Configure(new MetricsConfig
+            {
+                StatsdServerName = configuration.Hostname,
+                StatsdServerPort = configuration.Port,
+                Prefix = configuration.MetricsPrefix
+            });
+        }
+        
+        public void Gauge(string metricName, double value)
+        {
+            StatsdClient.Metrics.GaugeAbsoluteValue(metricName, value);
+        }
+
+        public void Counter(string metricName, int value)
+        {
+            StatsdClient.Metrics.Counter(metricName, value);
+        }
+
+        public void Timer(string metricName, int milliseconds)
+        {
+            StatsdClient.Metrics.Timer(metricName, milliseconds);
+        }
+    }
+}
